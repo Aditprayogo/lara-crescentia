fe76a09658287380be4f8a24272f418bca16492b
Aditprayogo <adit.ihzar@gmail.com>
2019-12-03 15:08:32 +0700

    create dashboard data dynamically

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\TravelPackage;
+use App\Transaction;
 
 class DashboardController extends Controller
 {
@@ -11,6 +13,17 @@ class DashboardController extends Controller
 	public function index()
 	{
 		# code...
-		return view('pages.admin.dashboard');
+		$paketTravel = TravelPackage::count();
+		$transaksi = Transaction::count();
+		$pending = Transaction::where('transaction_status', 'PENDING')->count();
+		$sukses = Transaction::where('transaction_status', 'SUCCESS')->count();
+
+
+		return view('pages.admin.dashboard',[
+			'paketTravel' => $paketTravel,
+			'transaksi' => $transaksi,
+			'pending' => $pending,
+			'sukses' => $sukses
+		]);
 	}
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -21,7 +21,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Paket Travel</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $paketTravel }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-hotel fa-2x text-gray-300"></i>
@@ -38,7 +38,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Transaksi</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaksi }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -57,7 +57,7 @@
                 <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pending</div>
                 <div class="row no-gutters align-items-center">
                   <div class="col-auto">
-                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0</div>
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $pending }}</div>
                   </div>
                   <div class="col">
                     {{-- <div class="progress progress-sm mr-2">
@@ -81,7 +81,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Sukses</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $sukses }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-check fa-2x text-gray-300"></i>
